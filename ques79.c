2aa9dbb50c4e070fc0952af56fdd9ab880acf07e
Bhuvika13 <nagpalbhuvika.171@gmail.com>
2025-11-26 23:52:23 +0530

    ques79.c

--- ques79.c
+++ ques79.c
@@ -1,26 +1,44 @@
-//Find the sum of main diagonal elements for a square matrix.
+//Perform diagonal traversal of a matrix.
 #include <stdio.h>
 
 int main() {
-    int n, sum = 0;
+    int rows, cols;
 
-    printf("Enter the size of the square matrix: ");
-    scanf("%d", &n);
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
 
-    int matrix[n][n];
+    printf("Enter the number of columns: ");
+    scanf("%d", &cols);
+
+    int matrix[rows][cols];
 
     printf("Enter the elements of the matrix:\n");
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
             scanf("%d", &matrix[i][j]);
         }
     }
 
-    for (int i = 0; i < n; i++) {
-        sum += matrix[i][i];
+    printf("Diagonal traversal of the matrix:\n");
+
+    for (int startCol = 0; startCol < cols; startCol++) {
+        int i = 0, j = startCol;
+        while (i < rows && j >= 0) {
+            printf("%d ", matrix[i][j]);
+            i++;
+            j--;
+        }
     }
 
-    printf("Sum of main diagonal elements: %d\n", sum);
+    for (int startRow = 1; startRow < rows; startRow++) {
+        int i = startRow, j = cols - 1;
+        while (i < rows && j >= 0) {
+            printf("%d ", matrix[i][j]);
+            i++;
+            j--;
+        }
+    }
 
+    printf("\n");
     return 0;
 }
