71ad938e7a73072f40f2a99b6d0d213661536baa
Bhuvika13 <nagpalbhuvika.171@gmail.com>
2025-11-26 23:38:19 +0530

    Create ques66.c

---
+++ ques66.c
@@ -0,0 +1,35 @@
+//Insert an element in a sorted array at the appropriate position.
+#include <stdio.h>
+
+int main() {
+    int n, key, i;
+
+    printf("Enter the number of elements in the sorted array: ");
+    scanf("%d", &n);
+
+    int arr[n + 1];  
+
+    printf("Enter %d elements in ascending order:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the element to insert: ");
+    scanf("%d", &key);
+
+    i = n - 1;
+    while (i >= 0 && arr[i] > key) {
+        arr[i + 1] = arr[i];
+        i--;
+    }
+    arr[i + 1] = key; 
+    n++;  
+
+    printf("Array after insertion:\n");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
